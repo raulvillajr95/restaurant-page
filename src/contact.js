9f181ac5d0cca9a2f85b28c6addfe4e413ee3efc
Raul <raulvillajr95@gmail.com>
2022-11-17 17:42:42 -0800

    Add buttons to main page

---
+++ src/contact.js
@@ -0,0 +1,25 @@
+const content = document.querySelector('div#content')
+
+function contact() {
+  let img = document.createElement('img')
+  img.setAttribute('src', "https://images.squarespace-cdn.com/content/v1/5e78653ea96b4e646ba71d8f/1592841951202-CNFCE5UK5HRNI6GXM5V6/headernoshadow.jpg")
+  img.setAttribute('alt', "Welcome. We're so glad you're here!")
+  content.appendChild(img)
+
+  let h1 = document.createElement('h1')
+  h1.textContent = 'Contact Us';
+  content.appendChild(h1)
+
+  let ul = document.createElement('ul')
+  let li1 = document.createElement('li')
+  li1.textContent = "Phone: (800) 323 - 3434"
+  ul.appendChild(li1)
+  let li2 = document.createElement('li')
+  li2.textContent = "Fax: (800) 454 - 4545"
+  ul.appendChild(li2)
+  content.appendChild(ul)
+
+  return content
+}
+
+export {contact};
--- src/index.js
+++ src/index.js
@@ -1,5 +1,27 @@
 import { home } from "./home.js";
 import { menu } from "./menu.js";
+import { contact } from "./contact.js";
+
+const content = document.querySelector('div#content')
+
+function tabButtons() {
+  let homeBtn = document.createElement('button')
+  let menuBtn = document.createElement('button')
+  let contactBtn = document.createElement('button')
+
+  homeBtn.textContent = 'Home';
+  menuBtn.textContent = 'Menu';
+  contactBtn.textContent = 'Contact';
+
+  content.appendChild(homeBtn)
+  content.appendChild(menuBtn)
+  content.appendChild(contactBtn)
+
+  return content
+}
+
+tabButtons()
 
 home()
 menu()
+contact()
