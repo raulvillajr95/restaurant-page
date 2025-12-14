31a9b2c8af3713fbb3ba0408dca10373c9ed7b47
Raul <84646523+raulvillajr95@users.noreply.github.com>
2025-12-14 08:50:28 -0800

    Update logo in home.js

--- src/home.js
+++ src/home.js
@@ -15,7 +15,7 @@ function home() {
   p.style.color = "#141414"
   p.style.fontSize = "1.25rem"
 
-  img.setAttribute('src', "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fvignette4.wikia.nocookie.net%2Flogopedia%2Fimages%2F6%2F68%2FChick-fil-A_logo_2012.png%2Frevision%2Flatest%3Fcb%3D20130511041847&f=1&nofb=1&ipt=42510cac832c0ac5435e064c37d1144c9a8da37dc89aa05721010b22f44bf593&ipo=images")
+  img.setAttribute('src', "https://upload.wikimedia.org/wikipedia/commons/0/02/Chick-fil-A_Logo.svg")
   img.setAttribute('alt', "chick-fil-a logo")
   h1.textContent = 'Classic taste in every bite.';
   p.textContent = 'We are open 6 days a week, 24hrs hours a day, just to serve you!'
